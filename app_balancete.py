7db8a8bf2927b9e342e425c8371dd4d6c00d9f24
gabrielcontabilidade2-svg <gabrielcontabilidade2@gmail.com>
2026-08-26 21:04:54 -0300

    Update app_balancete.py

--- app_balancete.py
+++ app_balancete.py
@@ -9,7 +9,7 @@ from supabase import create_client, Client
 # ==============================================================================
 # CONFIGURAÇÕES E INICIALIZAÇÃO SUPABASE
 # ==============================================================================
-st.set_page_config(page_title="Balancete Paroquial", page_icon="📊", layout="wide")
+st.set_page_config(page_title="Balancete Paroquial", page_icon="📊", layout="centered")
 
 SENHA_MESTRA = "Igreja123"
 
